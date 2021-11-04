71ee3c7300ecf86d637256e505a35c5bd43e494f
Daris <daris.hodzic.dh@gmail.com>
2021-11-04 15:28:39 +0100

    cryptoApi and redux store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "89e5a1deb4msh308c52f76679feap1b4e47jsn06c0a19c2cfb",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com/stats";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
