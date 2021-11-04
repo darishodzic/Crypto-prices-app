ee7bacbaee67d019284a90f93ef526abeb2fcfc1
Daris <daris.hodzic.dh@gmail.com>
2021-11-04 14:41:10 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -40,8 +40,21 @@ const App = (props) => {
             </Switch>
           </div>
         </Layout>
+        <div className="footer">
+          <Typography.Title
+            level={5}
+            style={{ color: "white", textAlign: "center" }}
+          >
+            Crypto App <br />
+            All rights reserverd
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
-      <div className="footer"></div>
     </div>
   );
 };
