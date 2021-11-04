248b4b74498677d54f1859db11817e002770f922
Daris <daris.hodzic.dh@gmail.com>
2021-11-04 14:32:45 +0100

    routing and layout

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+const Cryptocurrencies = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, facilis
+      veniam. Repellendus quasi soluta odio quod corrupti aliquid recusandae
+      deserunt eum molestiae, minus quam autem, ut reiciendis dolorum laborum?
+      Dolores.
+    </div>
+  );
+};
+
+export default Cryptocurrencies;
