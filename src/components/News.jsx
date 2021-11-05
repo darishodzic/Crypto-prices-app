bef58a490477cc79c023d218e6d61356bb2d5b61
Daris <daris.hodzic.dh@gmail.com>
2021-11-05 13:45:58 +0100

    cryptoNewsApi

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Typography, Select, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
 
 const News = () => {
   return <div></div>;
---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoNewsHeaders = {
+  "x-bingapis-sdk": "true",
+  "x-rapidapi-host": "bing-news-search1.p.rapidapi.com",
+  "x-rapidapi-key": "89e5a1deb4msh308c52f76679feap1b4e47jsn06c0a19c2cfb",
+};
+
+const baseUrl = "https://bing-news-search1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
+
+export const cryptoNewsApi = createApi({
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`
+        ),
+    }),
+  }),
+});
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;
